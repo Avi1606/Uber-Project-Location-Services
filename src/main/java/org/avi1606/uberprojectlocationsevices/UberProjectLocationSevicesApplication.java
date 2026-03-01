fe528de0ebd5f4facb55faf1fff998743e15037b
Avikumar <aviiiprajapati16@gmail.com>
2026-03-01 12:39:32 +0530

    Initialize Uber Project Location Services with Spring Boot, Redis configuration, and project setup files.

---
+++ src/main/java/org/avi1606/uberprojectlocationsevices/UberProjectLocationSevicesApplication.java
@@ -0,0 +1,16 @@
+package org.avi1606.uberprojectlocationsevices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.Driver;
+
+@SpringBootApplication
+public class UberProjectLocationSevicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UberProjectLocationSevicesApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/org/avi1606/uberprojectlocationsevices/UberProjectLocationSevicesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.avi1606.uberprojectlocationsevices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberProjectLocationSevicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
